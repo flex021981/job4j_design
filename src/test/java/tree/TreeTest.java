bc593932f9f4b56beb4cd248a02e0a414df5162c
Алексей Захаров <flex021981@gmail.com>
2021-01-25 19:51:01 +0300

    fix errors Решение задачи: Блок /1. Структуры данных и алгоритмы./6. Tree /2. Добавить метод boolean isBinary() [#432720] - fix code style - add test

--- src/test/java/tree/TreeTest.java
+++ src/test/java/tree/TreeTest.java
@@ -9,11 +9,11 @@ public class TreeTest {
     @Test
     public void when6ElFindLastThen6() {
         Tree<Integer> tree = new Tree<>(1);
-        tree.add(1, 2);
-        tree.add(1, 3);
-        tree.add(1, 4);
-        tree.add(4, 5);
-        tree.add(5, 6);
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
+        assertTrue(tree.add(1, 4));
+        assertTrue(tree.add(4, 5));
+        assertTrue(tree.add(5, 6));
         assertThat(
                 tree.findBy(6).isPresent(),
                 is(true)
@@ -23,7 +23,7 @@ public class TreeTest {
     @Test
     public void when6ElFindNotExitThenOptionEmpty() {
         Tree<Integer> tree = new Tree<>(1);
-        tree.add(1, 2);
+        assertTrue(tree.add(1, 2));
         assertThat(
                 tree.findBy(7).isPresent(),
                 is(false)
@@ -33,13 +33,23 @@ public class TreeTest {
     @Test
     public void whenTreeIsBinary(){
         Tree<Integer> tree = new Tree<>(1);
-        tree.add(1, 2);
-        tree.add(1, 3);
-        tree.add(2, 4);
-        tree.add(2, 5);
-        tree.add(3, 6);
-        tree.add(3, 7);
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
+        assertTrue(tree.add(2, 4));
+        assertTrue(tree.add(2, 5));
+        assertTrue(tree.add(3, 6));
+        assertTrue(tree.add(3, 7));
         assertTrue(tree.isBinary());
-
+    }
+    @Test
+    public void whenTreeIsNotBinary(){
+        Tree<Integer> tree = new Tree<>(1);
+        assertTrue(tree.add(1, 2));
+        assertTrue(tree.add(1, 3));
+        assertTrue(tree.add(1, 4));
+        assertTrue(tree.add(2, 5));
+        assertTrue(tree.add(3, 6));
+        assertTrue(tree.add(3, 7));
+        assertFalse(tree.isBinary());
     }
 }
